77cd21673d31f0e31d9068b1d4176a766473fef5
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-21 20:40:17 +0530

    console-txtinput-value

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var btnTranslate = document.querySelector(".btn-translate");
 var output = document.querySelector(".output")
 
 // checking if the click event hapening-console clicked
-function clickHandler(){
-    console.log("clicked!")
+// function clickHandler(){
+//     console.log("clicked!")
+// }
+
+function clickHandler() {
+    console.log(txtInput.value)
 }
 
 btnTranslate.addEventListener("click", clickHandler);
