a0c0c08797e2097d3002407dfe6d0b34ec7790c6
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-21 21:11:00 +0530

    translate-done

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ var txtInput = document.querySelector(".txt-input");
 var btnTranslate = document.querySelector(".btn-translate");
 var output = document.querySelector(".output")
 
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+
+function getTranslateURL(inputText){
+    return serverURL + "?" + "text=" +inputText;
+}
+// console.log(getTranslateURL)
 // checking if the click event hapening-console clicked
 // function clickHandler(){
 //     console.log("clicked!")
-// }
+// // }
 
 function clickHandler() {
-    console.log(txtInput.value)
+    var inputText = txtInput.value
+    fetch(getTranslateURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        output.innerText=translatedText;
+    })
 }
 
 btnTranslate.addEventListener("click", clickHandler);
