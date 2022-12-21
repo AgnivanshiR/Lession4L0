130ef8bc5562ea57521208d790f4d005f75a68ae
AgnivanshiR <86912784+AgnivanshiR@users.noreply.github.com>
2022-12-21 22:27:08 +0530

    added-normal-fxn

--- app.js
+++ app.js
@@ -19,15 +19,48 @@ function errorhandler(error){
     alert("Some issue with API server, please try again later")
 }
 
+// function clickHandler() {
+//     var inputText = txtInput.value
+//     fetch(getTranslateURL(inputText))
+//     .then(response=>response.json())
+//     .then(json=>{
+//         var translatedText = json.contents.translated;
+//         output.innerText=translatedText;
+//     })
+//     .catch(errorhandler)
+// }
+
 function clickHandler() {
     var inputText = txtInput.value
+    // fetch(getTranslateURL(inputText))
+    // .then(response=>response.json())
+    // .then(json=>{
+    //     var translatedText = json.contents.translated;
+    //     output.innerText=translatedText;
+    // })
+    // .catch(errorhandler)
+    // .then(function responsHandler(response) {return response.json()
+    // })
+    // .then( function logJSON(){
+    //     var translatedText = logJSON.contents.translated;
+    //     output.innerText = translatedText;
+    // })
+
+    // normal function
     fetch(getTranslateURL(inputText))
-    .then(response=>response.json())
-    .then(json=>{
-        var translatedText = json.contents.translated;
-        output.innerText=translatedText;
+    .then(function(response) {
+      // Parse the response as JSON
+      return response.json();
     })
-    .catch(errorhandler)
+    .then(function(json) {
+      // Extract the translated text from the JSON object
+      var translatedText = json.contents.translated;
+
+      // Set the innerText of the output element
+      output.innerText = translatedText;
+    });
 }
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
